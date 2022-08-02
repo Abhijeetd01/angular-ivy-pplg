2785ac1e30f3acea31977dfa073f987e85df1aa5
Abhijeetd01 <abhijeetd720@gmail.com>
2022-08-02 16:00:28 +0530

    base home wrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -8,19 +7,14 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+  // email = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-
-
-
-
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+  name = 'Angular 13';
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 import { AmazonComponent } from './amazon/amazon.component';
 import { GoogleComponent } from './google/google.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 const routes: Routes = [
   { path: 'amazon', component: AmazonComponent },
   { path: 'google', component: GoogleComponent },
 ];
 
-
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
     MatToolbarModule,
     MatInputModule,
     RouterModule.forRoot(routes),
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    BrowserModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
     BrowserAnimationsModule,
   ],
   declarations: [AppComponent, HelloComponent],
