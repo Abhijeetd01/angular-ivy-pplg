242bfcd0ae49b488cd468dcc1d7d6ebea6f7b99d
Abhijeetd01 <abhijeetd720@gmail.com>
2022-08-02 16:23:12 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
 // import { FlexLayoutModule } from '@angular/flex-layout';
